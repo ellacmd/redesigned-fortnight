ce1be7cc7b4d18b0de6779cc0fc684dd4e7a1454
Ella <emmanuellatrust@gmail.com>
2021-03-23 16:26:45 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let colors = ['#000000', '#14213d', '#fca311', '#e5e5e5', '#ef476f'];
+
+document.getElementById('button').addEventListener('click', changeColor);
+
+function changeColor(){
+    let color = parseInt(Math.random()*colors.length);
+    document.querySelector('body').style.backgroundColor = colors[color];
+}
