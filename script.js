443cab46adc0966a807fb461e1fa26917fcfea3f
Ella <emmanuellatrust@gmail.com>
2021-03-23 16:47:30 -0700

    make button change along with background color

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ document.getElementById('button').addEventListener('click', changeColor);
 function changeColor(){
     let color = parseInt(Math.random()*colors.length);
     document.querySelector('body').style.backgroundColor = colors[color];
+    document.querySelector('button').style.backgroundColor = colors[color];
 }
